6fc74b014825c595ed658fc1388eac7b699fa0b2
KutuevVladimir <VladimirKutuev@gmail.com>
2020-09-17 14:11:57 +0300

    Added mocked tests

--- src/main.c
+++ src/main.c
@@ -1,44 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 #include "sort.h"
 
-/* If this is being built for a unit test. */
-#ifdef UNIT_TESTING
-/* Redirect printf to a function in the test application so it's possible to
- * test the standard output. */
-#ifdef printf
-#undef printf
-#endif /* printf */
-extern int example_test_printf(const char *format, ...);
-#define printf example_test_printf
 
-#ifdef scanf
-#undef scanf
-#endif /* scanf */
-extern int example_test_scanf(const char *format, ...);
-#define scanf example_test_scanf
-
-
-/* Redirect fprintf to a function in the test application so it's possible to
- * test error messages. */
-#ifdef fprintf
-#undef fprintf
-#endif /* fprintf */
-#define fprintf example_test_fprintf
-extern int example_test_fprintf(FILE * const file, const char *format, ...);
-
-int example_main(int argc, char *argv[]);
-/* main is defined in the unit test so redefine name of the the main function
- * here. */
-#define main example_main
-
-/* All functions in this object need to be exposed to the test application,
- * so redefine static to nothing. */
-#define static
-
-
-#endif /* UNIT_TESTING */
 /**  Bubble sort
 
 int sort(int *arr, int size) {
@@ -97,9 +63,8 @@ static void parse(char *str) {
 }
 
 int main(int argc, char *argv[]) {
-
     // Parsing argv
-    char argv_concat[1000];
+    char argv_concat[1000] = {0};
     // if no argument -> return -1
     if (argc == 1){
         return -1;
